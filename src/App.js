9e9b75050dfe752a69d65043a65b17e6abeaac72
IvanStrokin <strokinIv23@yandex.ru>
2023-07-11 15:00:08 +0400

    1459

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
           <Routes>
-            <Route path='' element={[]}/>
+            <Route path='' element={[<Header/>]}/>
           </Routes>
       </div>
     </BrowserRouter>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,13 +6,29 @@ const Header = () => {
     return (
         <div className={s.Header}>
             <h1 className={s.Logo}>
-                LADA
+                <NavLink to="/">
+                    <img src='https://bogatyr.club/uploads/posts/2023-03/1678514164_bogatyr-club-p-avtovaz-logo-foni-instagram-49.jpg' alt="logo" />
+                </NavLink>
             </h1>
-            <div className={s.Products}>
+            <div className={s.ProductsBlock}>
                 <NavLink to="">Модельный ряд</NavLink>
                 <NavLink to="">Конфигуратор</NavLink>
             </div>
-            
+            <div className={s.InfoBlock}>
+                <NavLink to="">Диллеры</NavLink>
+                <NavLink to="">Контакты</NavLink>
+            </div>
+            <div className={s.Geoposition}>
+                <img src="https://static.tildacdn.com/tild3936-6530-4538-a432-306661633732/hotpngcom.png" />
+                <select name="choice">
+                    <option value="first" selected>Тольятти</option>
+                    <option value="second">Москва</option>
+                    <option value="third">Санкт-Питербург</option>
+
+                </select>
+            </div>
         </div>
     )
 }
+
+export default Header  
